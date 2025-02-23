8417e49ece4b48aaafabbdd0576592c9735e15c7
emine <eminegulgurses@gmail.com>
2025-02-23 13:08:01 +0300

    pro2

---
+++ src/main/java/task01/Task_1.java
@@ -0,0 +1,22 @@
+package task01;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+
+
+/*
++,-,*,/ islemleri Java'da matematikte kullanildigi gibi kullanilir
+Not 1: Java'da bir tamsayiyi baska bir tamsayiya bolerseniz sonuc her zaman tamsayi olur
+Not 2: Eger sonuc tamsayi degilse Java sonucun ondalik kismini silerek onu int'e donusturur.
+Not 3: Bir matematiksel islemde farkli data tiplerini kullanirsaniz sonuc her zaman buyuk data tipinde olur.
+ */
+
+        int amountOfMoney = 25;
+        int numberOfPeople = 10;
+        System.out.println(amountOfMoney / numberOfPeople);
+
+
+    }
+
+}
